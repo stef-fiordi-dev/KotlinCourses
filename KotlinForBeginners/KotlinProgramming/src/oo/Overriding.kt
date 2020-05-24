c448cf25855a24ce01d8a713451c4df33d795239
stef-fiordi-dev <fiordi.develop@gmail.com>
2020-05-25 00:56:03 +0200

    Added Overriding

---
+++ KotlinForBeginners/KotlinProgramming/src/oo/Overriding.kt
@@ -0,0 +1,27 @@
+package oo
+
+abstract class Course(val topic: String, val price: Double) {
+    open fun learn() {
+        println("Learning $topic.")
+    }
+}
+
+interface Learnable {
+    fun learn() {
+        println("Learning...")
+    }
+}
+
+open class KotlinCourse(): Learnable, Course("Kotlin", 999.99) {
+    final override fun learn() { //Cannot be overridden anymore
+        super<Course>.learn()
+        println("I'm one of the first people to learn Kotlin!")
+    }
+}
+
+
+
+fun main(args: Array<String>) {
+    val course = KotlinCourse()
+    course.learn()
+}
