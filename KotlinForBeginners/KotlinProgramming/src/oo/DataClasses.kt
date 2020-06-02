ba334ba5a4ebe28a9973dde1750fb6b09191fd0b
stef-fiordi-dev <fiordi.develop@gmail.com>
2020-06-02 16:32:21 +0200

    Added hexa and binary

--- KotlinForBeginners/KotlinProgramming/src/oo/DataClasses.kt
+++ KotlinForBeginners/KotlinProgramming/src/oo/DataClasses.kt
@@ -8,24 +8,24 @@ data class DataBook(val title: String, val author: String, val publicationYear: 
 
 }
 
-fun main(args: Array<String>) {
-    val book = Book("Super Book", "John Doe", 2017, 99.99)
-    val book2 = Book("Super Book", "John Doe", 2017, 99.99)
-    val dataBook = DataBook("Super Book", "John Doe", 2017, 99.99)
-    val dataBook2 = DataBook("Super Book", "John Doe", 2017, 99.99)
-    val dataBook3 = dataBook.copy()
-
-    val (title, author, year, price) = dataBook
-    val set = hashSetOf(dataBook, dataBook2, dataBook3)
-    val set2 = hashSetOf(book, book2)
-    println(set)
-    println(set2)
-
-    println(book)
-    println(dataBook)
-    println(dataBook3)
-    println(author)
-
-    println(book.equals(book2))
-    println(dataBook.equals(dataBook2))
-}
+//fun main(args: Array<String>) {
+//    val book = Book("Super Book", "John Doe", 2017, 99.99)
+//    val book2 = Book("Super Book", "John Doe", 2017, 99.99)
+//    val dataBook = DataBook("Super Book", "John Doe", 2017, 99.99)
+//    val dataBook2 = DataBook("Super Book", "John Doe", 2017, 99.99)
+//    val dataBook3 = dataBook.copy()
+//
+//    val (title, author, year, price) = dataBook
+//    val set = hashSetOf(dataBook, dataBook2, dataBook3)
+//    val set2 = hashSetOf(book, book2)
+//    println(set)
+//    println(set2)
+//
+//    println(book)
+//    println(dataBook)
+//    println(dataBook3)
+//    println(author)
+//
+//    println(book.equals(book2))
+//    println(dataBook.equals(dataBook2))
+//}
