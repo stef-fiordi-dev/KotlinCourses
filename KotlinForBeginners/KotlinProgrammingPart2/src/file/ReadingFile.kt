99e7ddfb9e97c8377dbadca4cb7cf13e5e769b7b
stef-fiordi-dev <fiordi.develop@gmail.com>
2020-06-14 17:46:04 +0200

    Added File reading and Maps

---
+++ KotlinForBeginners/KotlinProgrammingPart2/src/file/ReadingFile.kt
@@ -0,0 +1,11 @@
+package file
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    var lineNumber = 0
+    File("src/inputfile.txt").forEachLine {
+        ++lineNumber
+        println("#$lineNumber: $it")
+    }
+}
